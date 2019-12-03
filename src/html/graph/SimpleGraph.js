989ca2ec632f91afd00086f02355eb641afe8292
Kitty Jose <kitty@datachemist.com>
2019-12-03 14:37:20 +0000

    small fix

--- src/html/graph/SimpleGraph.js
+++ src/html/graph/SimpleGraph.js
@@ -3,7 +3,7 @@ const GraphResultsViewer = require('./GraphResultsViewer');
 
 
 function SimpleGraph(){
-	this.holder = document.createElement("div");	
+	this.holder = document.createElement("div");
 }
 
 SimpleGraph.prototype.setResult = function(result){
@@ -22,10 +22,10 @@ SimpleGraph.prototype.render = function(wgraph){
 
 
 SimpleGraph.prototype.getGraphDOM = function(){
-	this.gviz = new GraphResultsViewer(this.graph.config); 
+	this.gviz = new GraphResultsViewer(this.graph.config);
 	var gdom = this.gviz.getAsDOM();
-	var h = this.graph.config.height() ? this.graph.config.height() : 800;
-	var w = this.graph.config.width() ? this.graph.config.width() : 800;
+	var h = this.graph.config.height() ? this.graph.config.height() : 500;
+	var w = this.graph.config.width() ? this.graph.config.width() : 943;
 	gdom.setAttribute("style", "border: 1px solid #aaa; width: " + w + "px; height: " + h + "px; " + this.graph.config.width() + "px");
 	this.gviz.setData(this.graph);
 	this.gviz.initDOM(gdom);
