0d823f5e402d82267450551bfbe3c33ee658a0bc
francesca@datachemist.com <francesca@datachemist.com>
2019-12-09 10:03:51 +0000

    add export WOQL

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const TerminusUI = require('./TerminusUI');
 const RenderingMap = require('./client/RenderingMap');
+const WOQL =require('./viewer/WOQLRule')
 module.exports={TerminusUI:TerminusUI,
-                RenderingMap:RenderingMap}
+                RenderingMap:RenderingMap,
+            	WOQL:WOQL}
