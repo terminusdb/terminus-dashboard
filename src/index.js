4948909a6ae68f1e16d43d8d756baf259b6c2a24
francesca@datachemist.com <francesca@datachemist.com>
2020-01-06 13:22:55 +0000

    test travis

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const TerminusUI = require('./TerminusUI');
 
 const RenderingMap = require('./client/RenderingMap');
 const WOQL =require('./viewer/WOQLRule')
+
+
 module.exports={TerminusUI:TerminusUI,
                 RenderingMap:RenderingMap,
             	WOQL:WOQL}
