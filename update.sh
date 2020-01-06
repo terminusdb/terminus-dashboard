ddc32b6716c04ffa53d57a018208dcc331dcd659
francesca@datachemist.com <francesca@datachemist.com>
2020-01-06 16:16:17 +0000

    change updatesh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
+#setup_git() {
+ # git config --global user.email "travis@travis-ci.org"
+ # git config --global user.name "Travis CI"
+#}
 
 commit_website_files() {
   git add . dist/*
