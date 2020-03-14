1819c98b5cecb1f36ada7e0bef413700acfb5533
Kevin Feeney <kevin@datachemist.com>
2020-03-14 15:53:45 +0000

    Added tutorial integration to dashboard

---
+++ src/TerminusTutorial.js
@@ -0,0 +1,76 @@
+const UTILS = require("./Utils.js");
+const HTMLHelper = require('./html/HTMLHelper');
+
+function TerminusTutorialLoader(ui){
+	this.ui = ui;
+	this.fetchOptions();
+}
+
+TerminusTutorialLoader.prototype.fetchOptions = function(){
+	let cback = this.showTutorialChoices;
+	HTMLHelper.loadDynamicScript("tutorials", "https://terminusdb.github.io/terminus-tutorials/seshat-tutorial/script-of-queries.js", cback);
+}
+
+TerminusTutorialLoader.prototype.showTutorialChoices = function(){
+	alert("show choices")
+}
+
+
+TerminusTutorialLoader.prototype.getLabelDOM = function(){
+	return document.createTextNode("Connect");
+}
+
+TerminusTutorialLoader.prototype.getFormFieldDOM = function(ip, fname, ltxt, pholder){
+	var sci = document.createElement("div");
+	sci.setAttribute("class", "terminus-form-horizontal terminus-control-group terminus-form-field-spacing terminus-display-flex terminus-field-"+fname);
+	var lab = document.createElement("span");
+	lab.setAttribute("class", "terminus-control-label terminus-form-label terminus-form-field-label terminus-label-"+fname);
+	lab.appendChild(document.createTextNode(ltxt))
+	ip.setAttribute("type", "text");
+	ip.setAttribute("placeholder", pholder);
+	sci.appendChild(lab);
+	sci.appendChild(ip);
+	return sci;
+}
+
+TerminusTutorialLoader.prototype.getAsDOM = function(){
+	var scd = document.createElement("div");
+	scd.setAttribute("class", "terminus-form terminus-url-loader");
+	scd.appendChild(UTILS.getHeaderDom('Connect To Terminus Server'));
+	var mfd = document.createElement('div');
+	mfd.setAttribute('class', 'terminus-form-border terminus-margin-top');
+	scd.appendChild(mfd);
+	this.url_input = document.createElement("input");
+	this.url_input.setAttribute("class", "terminus-form-value terminus-form-url terminus-form-field-input terminus-input-text");
+	if(this.val){
+		this.url_input.value = this.val;
+	}
+	var dht = document.createElement('div');
+	dht.setAttribute('class', 'terminus-form-margin-top');
+	mfd.appendChild(dht);
+	if(!this.ui.attachedServer()){
+		mfd.appendChild(this.getFormFieldDOM(this.url_input, "connect", "URL", "Terminus DB URL"));
+	}
+	this.key_input = document.createElement("input");
+	this.key_input.setAttribute("class", "terminus-form-value terminus-value terminus-input-text");
+	mfd.appendChild(this.getFormFieldDOM(this.key_input, "connect", "Key", "Server API Key"));
+	var loadbuts = document.createElement("div");
+	loadbuts.setAttribute("class", "terminus-control-buttons");
+	var loadbut = document.createElement("button");
+	loadbut.setAttribute("class", "terminus-control-button terminus-url-load terminus-btn terminus-btn-float-right");
+	loadbut.appendChild(document.createTextNode("Connect"));
+	var self = this;
+	loadbut.addEventListener("click", function(){
+		let keyval = self.key_input.value;
+		let url = (self.ui.attachedServer() ? self.ui.attachedServer() : self.url_input.value); 
+		if(url){
+			self.ui.load(url, keyval);
+		}
+	});
+	loadbuts.appendChild(loadbut);
+	mfd.appendChild(loadbuts);
+	return scd;
+}
+
+
+module.exports=TerminusTutorialLoader
