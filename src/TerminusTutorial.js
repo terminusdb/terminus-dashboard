0ecb6a23dcce8d88ca5f5805de0bbf7520f28ac6
Kevin Feeney <kevin@datachemist.com>
2020-03-23 17:55:55 +0000

    adding tutorial into dashboard

--- src/TerminusTutorial.js
+++ src/TerminusTutorial.js
@@ -1,76 +1,177 @@
 const UTILS = require("./Utils.js");
 const HTMLHelper = require('./html/HTMLHelper');
 
+
 function TerminusTutorialLoader(ui){
 	this.ui = ui;
+	this.tutorial_server = "https://terminusdb.github.io/terminus-tutorials/";
 	this.fetchOptions();
+	this.container = document.createElement("span");
+	this.container.setAttribute("class", "terminus-main-page");
 }
 
 TerminusTutorialLoader.prototype.fetchOptions = function(){
-	let cback = this.showTutorialChoices;
-	HTMLHelper.loadDynamicScript("tutorials", "https://terminusdb.github.io/terminus-tutorials/seshat-tutorial/script-of-queries.js", cback);
+	let self = this;
+	let cback = function(){
+		self.showTutorialChoices()
+	}
+	HTMLHelper.loadDynamicScript("tutorials", this.tutorial_server + "tutorials.js", cback);
 }
+
+TerminusTutorialLoader.prototype.drawChoices = function(){
+	if(this.choices){
+		for(var i = 0 ; i < this.choices.length; i++){
+			this.showChoice(this.choices[i]);
+		}
+	}
+}
+
 
 TerminusTutorialLoader.prototype.showTutorialChoices = function(){
-	alert("show choices")
+	this.choices = getTutorialOptions();
+	this.drawChoices();
 }
 
 
-TerminusTutorialLoader.prototype.getLabelDOM = function(){
-	return document.createTextNode("Connect");
-}
-
-TerminusTutorialLoader.prototype.getFormFieldDOM = function(ip, fname, ltxt, pholder){
-	var sci = document.createElement("div");
-	sci.setAttribute("class", "terminus-form-horizontal terminus-control-group terminus-form-field-spacing terminus-display-flex terminus-field-"+fname);
-	var lab = document.createElement("span");
-	lab.setAttribute("class", "terminus-control-label terminus-form-label terminus-form-field-label terminus-label-"+fname);
-	lab.appendChild(document.createTextNode(ltxt))
-	ip.setAttribute("type", "text");
-	ip.setAttribute("placeholder", pholder);
-	sci.appendChild(lab);
-	sci.appendChild(ip);
-	return sci;
+TerminusTutorialLoader.prototype.showChoice = function(choice){
+	var hbox = document.createElement("div");
+	hbox.setAttribute("class", "terminus-welcome-box terminus-no-res-alert");
+	var ispan =  document.createElement("span");
+	ispan.setAttribute("class", "terminus-db-widget");
+	if(choice.css){
+		var ic = document.createElement("i");
+		ic.setAttribute("class", choice.css);
+		ispan.appendChild(ic);
+	}
+	hbox.appendChild(ispan);
+	var htit = document.createElement("span");
+	htit.appendChild(document.createElement("strong").appendChild(document.createTextNode(choice.title)));
+	htit.classList.add('terminus-welcome-title');
+	hbox.appendChild(htit);
+	var body = document.createElement("p");
+	body.setAttribute('class', 'terminus-welcome-body');
+	body.appendChild(document.createTextNode(choice.text));
+	hbox.appendChild(body);
+	let self = this
+	hbox.addEventListener("click", function(){
+		self.prepareDatabase(choice)
+	});
+	this.container.appendChild(hbox);	
 }
 
 TerminusTutorialLoader.prototype.getAsDOM = function(){
-	var scd = document.createElement("div");
-	scd.setAttribute("class", "terminus-form terminus-url-loader");
-	scd.appendChild(UTILS.getHeaderDom('Connect To Terminus Server'));
-	var mfd = document.createElement('div');
-	mfd.setAttribute('class', 'terminus-form-border terminus-margin-top');
-	scd.appendChild(mfd);
-	this.url_input = document.createElement("input");
-	this.url_input.setAttribute("class", "terminus-form-value terminus-form-url terminus-form-field-input terminus-input-text");
-	if(this.val){
-		this.url_input.value = this.val;
-	}
-	var dht = document.createElement('div');
-	dht.setAttribute('class', 'terminus-form-margin-top');
-	mfd.appendChild(dht);
-	if(!this.ui.attachedServer()){
-		mfd.appendChild(this.getFormFieldDOM(this.url_input, "connect", "URL", "Terminus DB URL"));
-	}
-	this.key_input = document.createElement("input");
-	this.key_input.setAttribute("class", "terminus-form-value terminus-value terminus-input-text");
-	mfd.appendChild(this.getFormFieldDOM(this.key_input, "connect", "Key", "Server API Key"));
-	var loadbuts = document.createElement("div");
-	loadbuts.setAttribute("class", "terminus-control-buttons");
-	var loadbut = document.createElement("button");
-	loadbut.setAttribute("class", "terminus-control-button terminus-url-load terminus-btn terminus-btn-float-right");
-	loadbut.appendChild(document.createTextNode("Connect"));
-	var self = this;
-	loadbut.addEventListener("click", function(){
-		let keyval = self.key_input.value;
-		let url = (self.ui.attachedServer() ? self.ui.attachedServer() : self.url_input.value); 
-		if(url){
-			self.ui.load(url, keyval);
-		}
-	});
-	loadbuts.appendChild(loadbut);
-	mfd.appendChild(loadbuts);
-	return scd;
+	HTMLHelper.removeChildren(this.container);
+	this.drawChoices();
+	return this.container;
 }
 
+TerminusTutorialLoader.prototype.prepareDatabase = function(choice){
+	this.client.deleteDatabase(choice.id)
+	.finally(()=>{
+		this.ui.CreateDatabase(choice)
+		.finally(()=>{
+			this.loadTutorial(choice)
+		})
+	})
+}
+
+//also need to update the UI screen
+TerminusTutorialLoader.prototype.loadTutorial = function(choice){
+	let self = this;
+	let cback = function(){
+		self.startTutorial(choice, TerminusTutorial);
+	}
+	HTMLHelper.loadDynamicScript(choice.id, this.tutorial_server + choice.id + "/tutorial.js", cback);
+}
+
+
+TerminusTutorialLoader.prototype.showTutorialLaunchPage = function(choice, tutorialConfig){
+	HTMLHelper.removeChildren(this.container);
+	//alert(tid)	
+	return this.container;
+}
+
+TerminusTutorialLoader.prototype.startTutorial = function(choice, tutorialConfig) {
+	this.showTutorialLaunchPage(choice, tutorialConfig)
+}
+
+TerminusTutorialLoader.prototype.createSchema = function(OWLSchema){
+    return this.client.updateSchema(OWLSchema)
+}
+
+TerminusTutorialLoader.prototype.insert = function(insertJSON){
+    return this.client.woql(insertJSON)
+}
+
+TerminusTutorialLoader.prototype.query = function(selectWOQL){
+    return this.client.woql(selectWOQL)
+}
+
+TerminusTutorialLoader.prototype.showTutorialStages = function(stages){
+	//
+	this.stages = []; 
+
+    return this.client.woql(InsertJSON)
+}
+
+TerminusTutorialLoader.prototype.loadTutorialStage = function(stage){
+	//
+
+    return this.client.woql(InsertJSON)
+}
+
+function getNextStageButton(func){
+    let but = document.createElement("button");
+    but.appendChild(document.createTextNode("Next Step"))
+    but.addEventListener("click", function(){
+        func();
+    })
+    return but;
+}
+
+TerminusTutorialLoader.prototype.executeStage = function(func){
+    let but = document.createElement("button");
+    but.appendChild(document.createTextNode("Run Query"))
+    but.addEventListener("click", function(){
+        func();
+    })
+    return but;
+}
+
+
+TerminusTutorialLoader.prototype.showStageResult = function(q, v, complete){
+    let vu = v || `let view = View.table()`;
+    var rq = Viewer.getQueryPane(eval(q), eval(vu), false, false, {showQuery: false});
+    let rd = rq.getAsDOM();
+    rq.load();    
+    return rd;
+}
+
+
+/**
+ * 
+ * @param {WOQLClient} client 
+ * @param {String} id 
+ * @param {String} title 
+ * @param {String} description 
+
+function createDatabase(id, title, description){
+    title = title || "Seshat";
+    description = description || "Seshat global history databank";
+    const dbdetails = {
+        "@context" : {
+            rdfs: "http://www.w3.org/2000/01/rdf-schema#",
+            terminus: "http://terminusdb.com/schema/terminus#"
+        },
+        "@type": "terminus:Database",
+        'rdfs:label' : { "@language":  "en", "@value": title },
+        'rdfs:comment': { "@language":  "en", "@value": description},
+        'terminus:allow_origin': { "@type" : "xsd:string", "@value" : "*" }
+    };
+    return WOQLclient.createDatabase(id, dbdetails);
+}
+
+
+ */
 
 module.exports=TerminusTutorialLoader
